d33afb7c6c2e22ee91e73d52f053d3840e96f126
Vansh Rana <vanshrana96669@gmail.com>
2025-09-15 02:44:43 +0530

    adding auth

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+const session = require("express-session");
+const flash = require("connect-flash");
 
+// Routes
 const listingRoutes = require("./routes/listings");
 const reviewRoutes = require("./routes/reviews");
-const session= require("express-session")
+const userRoutes = require("./routes/user");
+
 const MONGO_URL = "mongodb://127.0.0.1:27017/YOYO";
-const flash = require("connect-flash");
-app.use(flash());
 
 // Middleware
 app.set("view engine", "ejs");
@@ -21,43 +26,63 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 
-const sessionOptions ={
-    secret: "musuperscrectcode",
-    resave: false,
-    saveUninitialized: true,
- 
-    cookie:{
-
-        expires :Date.now()+7*24*60*60*1000,
-        maxAge:7*24*60*60*1000,
-        httpOnly:true,
-    },
-
+const sessionOptions = {
+  secret: "musuperscrectcode",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
 };
 
 app.use(session(sessionOptions));
-// MongoDB connection
-mongoose.connect(MONGO_URL)
-    .then(() => console.log("Connected to MongoDB"))
-    .catch(err => console.error("Connection error:", err));
+app.use(flash());
 
-//flash
-app.use((req,res,next)=>{
-    res.locals.success = req.flash("success");
-    next();
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// MongoDB connection
+mongoose
+  .connect(MONGO_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Connection error:", err));
+
+// flash middleware
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error"); // <-- ADD THIS for error messages
+  next();
 });
+
+// Demo route
+app.get("/demouser", async (req, res) => {
+  let fakeUser = new User({
+    email: "student@gmail.com",
+    username: "deta_student",
+  });
+  let registeredUser = await User.register(fakeUser, "hello word");
+  res.send(registeredUser);
+});
+
 // Routes
-app.use("/listings", listingRoutes);  // important: prefix is /listings
-app.use("/", reviewRoutes);           // review routes handle /listings/:id/reviews
+app.use("/listings", listingRoutes);
+app.use("/", reviewRoutes);
+app.use("/", userRoutes);
 
 app.get("/", (req, res) => {
-    res.redirect("/listings");  // redirect home to all listings
+  res.redirect("/listings");
 });
 
 // Error handler
 app.use((err, req, res, next) => {
-    const { statusCode = 500, message = "Something went wrong!" } = err;
-    res.status(statusCode).render("error", { err });
+  const { statusCode = 500, message = "Something went wrong!" } = err;
+  res.status(statusCode).render("error", { err });
 });
 
 // Start server
